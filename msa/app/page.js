786a76796e74b791e9f378881762ea6ad72e97b8
Your Name <you@example.com>
2023-11-20 21:28:27 +0300

    continue

--- msa/app/page.js
+++ msa/app/page.js
@@ -2,7 +2,7 @@ import Header from "@/components/header/header";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col lg:justify-center lg:max-w-[1024px]">
       <Header />
     </main>
   );
--- msa/components/header/header.jsx
+++ msa/components/header/header.jsx
@@ -4,24 +4,24 @@ import React from "react";
 const Header = () => {
   return (
     <div className="flex justify-between">
-      <div className="flex gap-x-1 max-w-1024px mt-6 ms-6">
-        <Image
+      <div className="flex gap-x-1  mt-6 ms-6">
+        {/* <Image
           src="/images/personal.png"
-          width={40}
+          width={60}
           height={40}
           className="rounded-full"
           alt="pers"
-        />
-        <div className="text-2xl mt-1 font-bold">Salih</div>
+        /> */}
+        <div className="text-3xl mt-1 font-bold">msa.</div>
       </div>
-      <div className="flex gap-x-1">
+      <div className="flex items-center gap-x-2 me-6 ">
         <Image src="/images/twitter-x.webp" width={40} height={40} alt="x" />
         <Image
           src="/images/icon-linkedin.svg"
           width={40}
           height={40}
           alt="linkedin"
-          className="bg-indigo-600"
+          className
         />
         <Image
           src="/images/icon-github.svg"
