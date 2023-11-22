fdb446d47d93bb986d6defdb7055e1549546d5a2
Your Name <you@example.com>
2023-11-22 16:54:05 +0300

    project part

--- msa/components/projects/projects.jsx
+++ msa/components/projects/projects.jsx
@@ -1,10 +1,23 @@
+"use client";
 import React from "react";
+import ProjectsData from "/assets/data/projects.json";
+import Image from "next/image";
 
 const Projects = () => {
   return (
     <div className="ms-6 me-6 mt-[3rem]">
       <div>
         <h1 className="text-5xl">Projects</h1>
+      </div>
+      <div>
+        {ProjectsData.map((project) => (
+          <Image
+            src={project.images}
+            alt={project.title}
+            width={90}
+            height={90}
+          />
+        ))}
       </div>
     </div>
   );
