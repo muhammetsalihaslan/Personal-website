95c1100d0c8b8060f2048aae1655d059ef691856
Your Name <you@example.com>
2023-11-22 17:42:08 +0300

    fixed

--- msa/components/projects/projects.jsx
+++ msa/components/projects/projects.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import ProjectsData from "/assets/data/projects.json";
 import Image from "next/image";
+import Tolink from "../link/tolink";
 
 const Projects = () => {
   return (
@@ -9,14 +10,27 @@ const Projects = () => {
       <div>
         <h1 className="text-5xl">Projects</h1>
       </div>
-      <div>
+      <div className="grid grid-cols-2">
         {ProjectsData.map((project) => (
-          <Image
-            src={project.images}
-            alt={project.title}
-            width={90}
-            height={90}
-          />
+          <div key={project.id} className="w-[535px] h-[500px]">
+            <Image
+              src={project.images}
+              alt={project.title}
+              width={535}
+              height={400}
+              className="object-cover"
+            />
+            <h1>{project.title}</h1>
+            <div>
+              {project.skills.map((skill, index) => (
+                <p key={index}>{skill}</p>
+              ))}
+            </div>
+            <div>
+              <Tolink toWhere="wiev project" />
+              <Tolink toWhere="wiev code" />
+            </div>
+          </div>
         ))}
       </div>
     </div>
