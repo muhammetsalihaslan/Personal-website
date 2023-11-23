83576acd4ac0103e680c00c60f99d2281e811320
Your Name <you@example.com>
2023-11-23 18:01:43 +0300

    project hover

--- msa/components/projects/projects.jsx
+++ msa/components/projects/projects.jsx
@@ -12,23 +12,16 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-2 mt-[3rem] gap-y-1 ms-6 lg:items-center ">
         {ProjectsData.map((project) => (
-          <div key={project.id} className="w-[535px] h-[300px]">
-            <Image
-              src={project.images}
-              alt={project.title}
-              width={450}
-              height={400}
-              className="peer cursor-pointer "
-            />
-
-            <div className="flex flex-col gap-y-1 ">
-              <h1 className="text-2xl">{project.title}</h1>
-              <div className="flex gap-x-2 ">
-                {project.skills.map((skill, index) => (
-                  <p key={index}>{skill}</p>
-                ))}
-              </div>
-              <div className="flex lg:invisible  gap-x-2 lg:w-[200px] lg:relative lg:-mt-[12rem] lg:flex lg:flex-col lg:items-center lg:ms-[7rem] lg:gap-y-2 lg:cursor-pointer lg:peer-hover:visible ">
+          <div key={project.id} className="w-[535px] h-[300px] group relative">
+            <div className="group">
+              <Image
+                src={project.images}
+                alt={project.title}
+                width={450}
+                height={400}
+                className="cursor-pointer lg:group-hover:opacity-50 "
+              />
+              <div className="flex gap-x-2 mt-[55px] absolute lg:w-[200px]  lg:-mt-[8rem] lg:flex lg:flex-col lg:items-center lg:ms-[7rem] lg:gap-y-2 lg:cursor-pointer lg:invisible lg:group-hover:visible  ">
                 <Tolink
                   toWhere="wiev project"
                   privateClass="underline underline-offset-4 decoration-sky-500"
@@ -37,6 +30,15 @@ const Projects = () => {
                   toWhere="wiev code"
                   privateClass="underline underline-offset-4 decoration-sky-500"
                 />
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-y-1 lg:relative   ">
+              <h1 className="text-2xl">{project.title}</h1>
+              <div className="flex gap-x-2 ">
+                {project.skills.map((skill, index) => (
+                  <p key={index}>{skill}</p>
+                ))}
               </div>
             </div>
           </div>
