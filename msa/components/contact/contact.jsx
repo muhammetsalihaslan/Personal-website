1d21842c8627094f1d7555474fa4788b8e03b2ae
Your Name <you@example.com>
2023-11-29 17:10:56 +0300

    sets

--- msa/components/contact/contact.jsx
+++ msa/components/contact/contact.jsx
@@ -1,8 +1,35 @@
-import React from "react";
-import Tolink from "../link/tolink";
+"use client";
+import React, { useState } from "react";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  const PUBLIC_KEY = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, formData, PUBLIC_KEY)
+      .then((response) => {
+        console.log("Email sent successfully:", response);
+      })
+      .catch((error) => {
+        console.log("Error sending email:", error);
+      });
+  };
+
   return (
     <div className="mt-10  grid grid-rows-2 md:grid-cols-2  justify-center text-center ms-6 me-6 gap-x-6 md:h-[25rem]">
       <div className="flex flex-col justify-center items-center md:mt-[13rem]  gap-y-6 ms-6">
@@ -13,25 +40,40 @@ const Contact = () => {
         </p>
       </div>
       <div className="md:mt-10">
-        <form action="submit" className="flex flex-col gap-y-6">
+        <form
+          action="submit"
+          className="flex flex-col gap-y-6"
+          onSubmit={handleSubmit}
+        >
           <input
             type="text"
+            name="name"
             className="p-3 bg-[#E5E4E7] outline-none border-b-2 border-black "
             placeholder="NAME"
+            onChange={handleChange}
+            value={formData.name}
           />
           <input
             type="email"
+            name="email"
             className="p-3 bg-[#E5E4E7] outline-none border-b-2 border-black "
             placeholder="EMAIL"
+            onChange={handleChange}
+            value={formData.email}
           />
           <textarea
             className="h-[10rem] p-3 outline-none bg-[#E5E4E7] border-b-2 border-black "
             placeholder="MESSAGE"
+            name="message"
+            onChange={handleChange}
+            value={formData.message}
           />
-          <Tolink
-            toWhere="send message"
-            privateClass="underline underline-offset-4 decoration-sky-500 hover:text-sky-500 text-xl max-w-fit flex justify-items-end"
-          />
+          <button
+            type="submit"
+            className="underline underline-offset-4 decoration-sky-500 hover:text-sky-500 text-xl max-w-fit flex justify-items-end"
+          >
+            send message
+          </button>
         </form>
       </div>
     </div>
